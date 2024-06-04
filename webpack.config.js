9a6c07e29ce405fea2f363c2e48a54d91656368b
YaSlovar_work <stanko.tsep@yandex.ru>
2024-06-04 17:51:27 +0700

    server-included

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ const {categories} = require('./categories')
 
 const canonicalURL = 'https://www.ssk22.ru'
 
+const HttpsProxyAgent = require('https-proxy-agent');
+const fetch1 = require('node-fetch');
+
 /*
 function generateArticleHtmlPlugins() {
   return bpages.map(articleData => {
@@ -51,109 +54,127 @@ const ROUTES = {
 
   about: '/about/'
 };
-
-module.exports = {
-  entry: { 
-    index: './src/pages/index.js', 
-    blogpage: './src/pages/blog-page.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].js',
-    assetModuleFilename: 'images/[hash][ext]',
-    //publicPath: ''
-  },
-    // добавили режим разработчика
-  mode: 'development',
-  devServer: {
-    static: path.resolve(__dirname, "./dist"), // путь, куда "смотрит" режим разработчика
-    compress: true, // это ускорит загрузку в режиме разработки
-    port: 8081, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-    open: true, // сайт будет открываться сам при запуске npm run dev
-  },
-  module: {
-    rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
-      {
-        // регулярное выражение, которое ищет все js файлы
-        test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
-        use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
-        exclude: '/node_modules/'
-      },
-      {
-        test: /favicon\.svg/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "",
+function generateConfig(oporyData) {
+  return {
+    entry: { 
+      index: './src/pages/index.js', 
+      blogpage: './src/pages/blog-page.js',
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/[name].js',
+      assetModuleFilename: 'images/[hash][ext]',
+      //publicPath: ''
+    },
+      // добавили режим разработчика
+    mode: 'development',
+    devServer: {
+      static: path.resolve(__dirname, "./dist"), // путь, куда "смотрит" режим разработчика
+      compress: true, // это ускорит загрузку в режиме разработки
+      port: 8081,
+      open: true, // сайт будет открываться сам при запуске npm run dev
+    },
+    module: {
+      rules: [ 
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: '/node_modules/'
+        },
+        {
+          test: /favicon\.svg/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "",
+              },
             },
-          },
-        ],
-      },
-      
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+          type: 'asset/resource',
+          exclude: [
+            path.resolve(__dirname, "./src/images/favicon.svg"),
+          ],
+        },
+        {
+          test: /\.css$/,
+          // при обработке этих файлов нужно использовать MiniCssExtractPlugin.loader и css-loader
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: { importLoaders: 1 },
+            },
+            // Добавьте postcss-loader
+            "postcss-loader",
+          ],
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        templateParameters: {
+          canonicalURL,
+          categories,
+          ROUTES
+        },
+        title: "СтанкоСтальКонструкция | Производство гранёных опор освещения",
+        template: './src/index.html', // путь к файлу index.html
+        chunks: ['index'],
+      }),
+      new HtmlWebpackPlugin({
+        templateParameters: {
+          canonicalURL,
+          ROUTES
+        },
+        title: "О производстве",
+        template: './src/about.html', // путь к файлу index.html
+        filename: 'about/index.html',
+        chunks: ['index'],
+      }),
+      new HtmlWebpackPlugin({
+        templateParameters: {
+          canonicalURL,
+          ROUTES,
+          tableData: oporyData.filter(i => i.type === 'sfg')
+        },
+        title: "Опоры силовые фланцевые граненые",
+        template: './src/category-page.html', // путь к файлу index.html
+        filename: 'opory-osveshcheniya/opory-silovye-flancevye-granenye/index.html',
+        chunks: ['index'],
+      }),
+      new HtmlWebpackPlugin({
+        templateParameters: {
+          canonicalURL,
+          ROUTES,
+         
+        },
+        title: "Опора МСО-ФГ-4",
+        template: './src/product-page.html', // путь к файлу index.html
+        filename: 'opory-osveshcheniya/opory-silovye-flancevye-granenye/mso-fg-4-sfg-400-c/index.html',
+        chunks: ['index'],
+      }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].css'
+      })
+    ], 
+  }
+}
+const proxyAgent = new HttpsProxyAgent.HttpsProxyAgent('http://10.10.14.14:3128');
 
-      {
-        // регулярное выражение, которое ищет все файлы с такими расширениями
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
-        exclude: [
-          path.resolve(__dirname, "./src/images/favicon.svg"),
-        ],
-      },
-      {
-        test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // MiniCssExtractPlugin.loader и css-loader
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: { importLoaders: 1 },
-          },
-          // Добавьте postcss-loader
-          "postcss-loader",
-        ],
-      },
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      templateParameters: {
-        canonicalURL,
-        categories,
-        ROUTES
-      },
-      title: "СтанкоСтальКонструкция | Производство гранёных опор освещения",
-      template: './src/index.html', // путь к файлу index.html
-      chunks: ['index'],
-    }),
-    new HtmlWebpackPlugin({
-      templateParameters: {
-        canonicalURL,
-        ROUTES
-      },
-      title: "О производстве",
-      template: './src/about.html', // путь к файлу index.html
-      filename: 'about/index.html',
-      chunks: ['index'],
-    }),
-    new HtmlWebpackPlugin({
-      templateParameters: {
-        canonicalURL,
-        ROUTES
-      },
-      title: "Опора МСО-ФГ-4",
-      template: './src/product-page.html', // путь к файлу index.html
-      filename: 'opory-osveshcheniya/opory-silovye-flancevye-granenye/mso-fg-4-sfg-400-c/index.html',
-      chunks: ['index'],
-    }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
-  ], 
+module.exports = () => {
+  return new Promise((resolve, reject) => {
+      Promise.all([
+          fetch1('https://functions.yandexcloud.net/d4e9aq1evmfdb0cc7uo4', { agent: proxyAgent}).then(res => res.json()), 
+        ])
+        .then((data) => {
+          resolve(generateConfig(data[0]));
+        })
+     
+  });
 }
